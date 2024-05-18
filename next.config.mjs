016e4bcf61d1c498735c39d3844f6ec06d0e88a4
Firet <damianfederico@protonmail.com>
2024-05-18 16:45:36 -0300

    Add configuration to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/vegan-volunteerism",
+    output: "export",
+    reactStrictMode: true,
+    distDir: "docs",
+};
 
 export default nextConfig;
