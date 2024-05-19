44a1657c996a359885f003ae0ebf7c4936ed61c2
giselle1990 <56566057+giselle1990@users.noreply.github.com>
2024-05-19 14:15:06 -0300

    modifique algo para ver si podia agregar el navbar como en el figma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
+import React from 'react';
+import '../styles/navbar.css'; // Asegúrate de que la ruta sea correcta
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <nav className="navbar w-full flex justify-between items-center">
+        <div className="flex justify-center w-full lg:w-auto">
+          <a href="#home">Ser voluntario</a>
+          <a href="#about">Donar</a>
+          <a href="#contact">Carga tu ONG</a>
+          <a href="#about">Quienes somos</a>
+        </div>
+      </nav>
+      <h1 className="page-title">
+        App voluntariado vegano
+      </h1>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <h1 className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          App voluntariado vegano
-        </h1>
+        {/* Aquí puedes añadir el contenido principal de la página */}
       </div>
     </main>
-  );
+  )
 }
