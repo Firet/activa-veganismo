f9715f24cd0bd8e55ff89750d9572770ed150366
Firet <damianfederico@protonmail.com>
2024-05-20 23:12:45 -0300

    Remove all from  next.config (except output:export) and add the build folder created

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/vegan-volunteerism',
-    assetPrefix: '/vegan-volunteerism/',
     output: "export",
-    reactStrictMode: true,
-    // distDir: "docs",
 };
 
 export default nextConfig;
