c4ac28320fe42b1db6104a8db52dde5631926d67
Firet <damianfederico@protonmail.com>
2024-05-20 22:03:51 -0300

    Move build folder to root and change next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/docs',
+    basePath: '',
     assetPrefix: '',
     output: "export",
     reactStrictMode: true,
-    distDir: "docs",
+    // distDir: "docs",
 };
 
 export default nextConfig;
