73126c1ac1b2a3f39f0d5fd779c051fdd78e02a1
Firet <damianfederico@protonmail.com>
2024-05-23 12:44:00 -0300

    Create UnderConstruction component

---
+++ src/components/under-construction/under-construction.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function UnderConstruction() {
+	return (
+		<h1 className="flex flex-col space-evenly items-center text-5xl mt-16">
+			🏗️Esta sección está en construcción🚧
+		</h1>
+	);
+}
