30f4962b9af7a424e8d8940cb9a49820d537e898
Firet <damianfederico@protonmail.com>
2024-05-24 16:36:07 -0300

    Append vegan-volunteerism/ to logo path

--- src/components/organization-card/organization-card.tsx
+++ src/components/organization-card/organization-card.tsx
@@ -7,47 +7,11 @@ export default function OrganizationCard() {
 
     const organizations: Organization[] = [
         {
-            id: 1,
-            name: 'Voicot',
-            description:
-                'Organización artística.',
-            logo: '/icon.png',
-            website: 'https://www.voicot.com/',
-            donationUrl: 'https://www.voicot.com/donaciones',
-        },
-        {
-            id: 2,
-            name: 'Difusión V',
-            description:
-                'Difunde Veganismo.',
-            logo: '/difu-v.JPG',
-            website: 'https://www.difusionv.com',
-            donationUrl: 'https://www.unicef.org/argentina/donar',
-        },
-        {
-            id: 3,
-            name: 'Amigues por las calles',
-            description:
-                'Organización de voluntaries que proveen comida vegana a la gente en situación de calle.',
-            logo: '/amigues-sin-fondo.png',
-            website: 'https://www.instagram.com/amiguesporlascalles/',
-            donationUrl: 'https://www.instagram.com/amiguesporlascalles/',
-        },
-        {
-            id: 4,
-            name: 'Amigues por las calles',
-            description:
-                'Organización de voluntaries que proveen comida vegana a la gente en situación de calle.',
-            logo: './amigues-sin-fondo.png',
-            website: 'https://www.instagram.com/amiguesporlascalles/',
-            donationUrl: 'https://www.instagram.com/amiguesporlascalles/',
-        },
-        {
             id: 5,
             name: 'Amigues por las calles',
             description:
                 'Organización de voluntaries que proveen comida vegana a la gente en situación de calle.',
-            logo: '../amigues-sin-fondo.png',
+            logo: 'vegan-volunteerism/amigues-sin-fondo.png',
             website: 'https://www.instagram.com/amiguesporlascalles/',
             donationUrl: 'https://www.instagram.com/amiguesporlascalles/',
         },
