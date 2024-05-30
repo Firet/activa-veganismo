7b0f9dc75c17f099a020927b08d48fe7912091dc
Giselle San German <gisellesang@hotmail.com>
2024-05-30 14:44:51 -0300

    Agregue formulario de Carga tu ONG

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
+'use client';
+
 import React from 'react';
 import OrganizationCard from '@/components/organization-card/organization-card';
 
 export default function Home() {
-
-	return (
-		<>
-			<OrganizationCard />
-		</>
-	);
+  return (
+    <>
+      <OrganizationCard />
+    </>
+  );
 }
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,18 +1,20 @@
+'use client';
+
 import React from 'react';
-import './header.css';
 import Link from 'next/link';
+import './header.css';
 
 export default function Header() {
-	return (
-		<header>
-			<nav className="navbar flex flex-col space-evenly items-center">
-				<div>
-					<a href="#home">Ser voluntario</a>
-					<a href="#about">Donar</a>
-					<a href="#contact">Carga tu ONG</a>
-					<Link href="/quienes-somos">Quienes somos</Link>
-				</div>
-			</nav>
-		</header>
-	);
+  return (
+    <header>
+      <nav className="navbar flex flex-col space-evenly items-center">
+        <div>
+          <a href="#home">Ser voluntario</a>
+          <a href="#about">Donar</a>
+          <Link href="/cargar-ong">Carga tu ONG</Link>
+          <Link href="/quienes-somos">Quienes somos</Link>
+        </div>
+      </nav>
+    </header>
+  );
 }
