ce00ba1c694c615809c17b37ea690da22e43b619
Firet <damianfederico@protonmail.com>
2024-06-04 17:18:42 -0300

    Create Map component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,31 +2,13 @@
 
 import React from 'react';
 import OrganizationCard from '@/components/organization-card/organization-card';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
-
-const containerStyle = {
-	width: '100%',
-	height: '400px',
-};
-
-const center = {
-	lat: -34.603722, // Latitud de Buenos Aires
-	lng: -58.381592, // Longitud de Buenos Aires
-};
+import Map from '@/components/map/map'
 
 export default function Home() {
 	return (
 		<>
 			<OrganizationCard />
-			<LoadScript googleMapsApiKey="AIzaSyAno2Y5lzSsxzKzML7FJjpmyzkUpuRMRyw">
-				<GoogleMap
-					mapContainerStyle={containerStyle}
-					center={center}
-					zoom={12}
-				>
-					<></>
-				</GoogleMap>
-			</LoadScript>
+			<Map />
 		</>
 	);
 }
