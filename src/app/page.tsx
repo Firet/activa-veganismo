6a76909c8ee887204f5ad147325f09919070d143
Firet <damianfederico@protonmail.com>
2024-06-07 17:29:55 -0300

    Create CustomForm component and move ong-form to that component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import OrganizationCard from '@/components/organization-card/organization-card';
-import Map from '@/components/map/map'
+import Map from '@/components/map/map';
 
 export default function Home() {
 	return (
--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -2,30 +2,27 @@ import React from 'react';
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 const containerStyle = {
-    width: '100%',
-    height: '400px'
+	width: '100%',
+	height: '400px',
 };
 
 const center = {
-    lat: -34.603722, // Latitud de Buenos Aires
-    lng: -58.381592  // Longitud de Buenos Aires
+	lat: -34.603722, // Latitud de Buenos Aires
+	lng: -58.381592, // Longitud de Buenos Aires
 };
 
-
 export default function Map() {
-
-    return (
-        <>
-            <LoadScript googleMapsApiKey="AIzaSyAno2Y5lzSsxzKzML7FJjpmyzkUpuRMRyw">
-                <GoogleMap
-                    mapContainerStyle={containerStyle}
-                    center={center}
-                    zoom={12}
-                >
-                    { /* Child components, such as markers, info windows, etc. */}
-                </GoogleMap>
-            </LoadScript>
-
-        </>
-    );
+	return (
+		<>
+			<LoadScript googleMapsApiKey="AIzaSyAno2Y5lzSsxzKzML7FJjpmyzkUpuRMRyw">
+				<GoogleMap
+					mapContainerStyle={containerStyle}
+					center={center}
+					zoom={12}
+				>
+					{/* Child components, such as markers, info windows, etc. */}
+				</GoogleMap>
+			</LoadScript>
+		</>
+	);
 }
