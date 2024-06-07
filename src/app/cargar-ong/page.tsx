42e349da84a8382f5b96e76879b22ffef69240d0
Firet <damianfederico@protonmail.com>
2024-06-07 17:41:41 -0300

    Rename cargarONG to Page and make it a tsx file

--- src/app/cargar-ong/page.tsx
+++ src/app/cargar-ong/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import CustomForm from '@/components/custom-form/custom-form';
 
-export default function CargarONG() {
+export default function Page() {
 	return (
 		<div>
 			<h1>Cargá tu ONG</h1>
