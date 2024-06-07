c0bc5753564fbc041f08222bf75c401ecffbfe62
Firet <damianfederico@protonmail.com>
2024-06-07 18:00:23 -0300

    Remove wrong link to / in quienes-somos

--- src/app/quienes-somos/page.tsx
+++ src/app/quienes-somos/page.tsx
@@ -4,9 +4,7 @@ import Link from 'next/link';
 export default function Page() {
 	return (
 		<>
-			<h1>Quienes Somos</h1>
 			<UnderConstruction />
-			<Link href="/">Volver al Inicio</Link>
 		</>
 	);
 }
