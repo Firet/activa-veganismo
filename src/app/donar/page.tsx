d8d67c6cb43da7d6bbaf1abf95ac1b2fc9761321
Firet <damianfederico@protonmail.com>
2024-06-07 18:06:21 -0300

    Create donar page and add link in header

---
+++ src/app/donar/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import UnderConstruction from '@/components/under-construction/under-construction';
+
+export default function Page() {
+    return (
+        <div>
+            <UnderConstruction />
+        </div>
+    );
+}
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
 			<nav className="navbar flex flex-col space-evenly items-center">
 				<div>
 					<Link href="/">Inicio</Link>
-					<a href="#about">Donar</a>
+					<Link href="/donar">Donar</Link>
 					<Link href="/cargar-ong">Cargá tu ONG</Link>
 					<Link href="/quienes-somos">Quienes somos</Link>
 				</div>
