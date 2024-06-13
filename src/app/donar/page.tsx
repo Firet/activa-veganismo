210b21225ff21b7eb11e5e72ed38473127b5c952
Firet <damianfederico@protonmail.com>
2024-06-13 11:06:30 -0300

    Apply prettier

--- src/app/donar/page.tsx
+++ src/app/donar/page.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import UnderConstruction from '@/components/under-construction/under-construction';
 
 export default function Page() {
-    return (
-        <div>
-            <UnderConstruction />
-        </div>
-    );
+	return (
+		<div>
+			<UnderConstruction />
+		</div>
+	);
 }
