35c882297860bf076c124cfd77286805788e24c1
Firet <damianfederico@protonmail.com>
2024-06-14 07:23:39 -0300

    Add fake data in usePost

--- src/hooks/usePost.tsx
+++ src/hooks/usePost.tsx
@@ -1,18 +1,28 @@
 import axios from 'axios';
 
-const usePost = () => {
-	const url = ``;
+const usePost = (data: any) => {
+	const url = `https://jsonplaceholder.typicode.com/posts`;
+	const fakeData = {
+		method: 'POST',
+		body: JSON.stringify({
+			title: 'foo',
+			body: 'bar',
+			userId: 1,
+		}),
+		headers: {
+			'Content-type': 'application/json; charset=UTF-8',
+		},
+	};
 
-	const postData = async () => {
+	const handlePost = async () => {
 		try {
-			const response = await axios.post(url);
+			const response = await axios.post(url, fakeData);
 			const responseData = response.data;
 		} catch (error) {
 			console.error(error);
 		}
 	};
-
-	postData();
+	handlePost();
 };
 
 export default usePost;
