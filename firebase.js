53807ed7639e638f08de95aa4944fc43b45f6a47
Giselle San German <gisellesang@hotmail.com>
2024-06-21 20:47:40 -0300

    Adicion de archivo configuracion

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDs3MVh3fpvj4INOlSD6AipnD0yi3o2oo4",
+  authDomain: "activa-veganismo-51b9a.firebaseapp.com",
+  projectId: "activa-veganismo-51b9a",
+  storageBucket: "activ-veganismo-51b9a.appspot.com",
+  messagingSenderId: "228219423905",
+  appId: "1:228219423905:web:62b741b0b2d2dd1d8db4e0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
