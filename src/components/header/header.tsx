7d54cf9fa9a88af86303b32c4c5576b66a60a891
Firet <damianfederico@protonmail.com>
2024-08-19 20:38:41 -0300

    Add logo to header and add border bottom red and white letters

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,19 +1,27 @@
 'use client';
 
 import React from 'react';
+import Image from 'next/image';
 import Link from 'next/link';
 import './header.css';
 
 export default function Header() {
 	return (
 		<header>
-			<nav className="navbar flex flex-col space-evenly items-center">
-				<div>
-					<Link href="/">Inicio</Link>
-					<Link href="/donar">Donar</Link>
-					<Link href="/cargar-ong">Cargá tu ONG</Link>
-					<Link href="/quienes-somos">Quienes somos</Link>
-				</div>
+			<div className="image-container">
+				<Image
+					src='./activa-veganismo.png'
+					alt="Logo activá veganismo"
+					unoptimized
+					width={125}
+					height={125}
+				/>
+			</div>
+			<nav className="navbar flex flex-row justify-around items-center">
+				<Link href="/">Inicio</Link>
+				<Link href="/donar">Donar</Link>
+				<Link href="/cargar-ong">Cargá tu ONG</Link>
+				<Link href="/quienes-somos">Quienes somos</Link>
 			</nav>
 		</header>
 	);
