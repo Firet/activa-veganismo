5182b86ff109fe156e4426b1ab00f3d72231efba
Firet <damianfederico@protonmail.com>
2024-08-20 14:27:38 -0300

    Create Hero Section component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import React from 'react';
+import HeroSection from '@/components/hero-section/hero-section';
 import OrganizationCard from '@/components/organization-card/organization-card';
 import Map from '@/components/map/map';
 
 export default function Home() {
 	return (
 		<>
+			<HeroSection />
 			<OrganizationCard />
 			<Map />
 		</>
