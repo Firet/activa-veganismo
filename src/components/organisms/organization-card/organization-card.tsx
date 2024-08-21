08b6866c5d6bf18eb5a490647378d79d61df71d3
Firet <damianfederico@protonmail.com>
2024-08-21 01:06:17 -0300

    Import organization data from organization constant in organization-card component

--- src/components/organisms/organization-card/organization-card.tsx
+++ src/components/organisms/organization-card/organization-card.tsx
@@ -1,70 +1,10 @@
 import React from 'react';
-import Organization from '@/interfaces/organization';
+import organizations from '../../../constants/organization';
 import Image from 'next/image';
 import './organization-card.css';
 
 export default function OrganizationCard() {
-	const organizations: Organization[] = [
-		{
-			id: 1,
-			name: 'Voicot',
-			description: 'Organización artística.',
-			logo: './voicot.png',
-			website: 'https://www.voicot.com/',
-			donationUrl: 'https://www.voicot.com/donaciones',
-		},
-		{
-			id: 2,
-			name: 'Difusión V',
-			description: 'Difunde Veganismo.',
-			logo: './difu-v.png',
-			website: 'https://www.difusionv.com',
-			donationUrl: 'https://www.unicef.org/argentina/donar',
-		},
-		{
-			id: 3,
-			name: 'Amigues por las calles',
-			description:
-				'Organización de voluntaries que proveen comida vegana a la gente en situación de calle.',
-			logo: './amigues-sin-fondo.png',
-			website: 'https://www.instagram.com/amiguesporlascalles/',
-			donationUrl: 'https://www.instagram.com/amiguesporlascalles/',
-		},
-		{
-			id: 4,
-			name: 'Difusión V',
-			description: 'Difunde Veganismo.',
-			logo: './difu-v.png',
-			website: 'https://www.difusionv.com',
-			donationUrl: 'https://www.unicef.org/argentina/donar',
-		},
-		{
-			id: 5,
-			name: 'Amigues por las calles',
-			description:
-				'Organización de voluntaries que proveen comida vegana a la gente en situación de calle.',
-			logo: './amigues-sin-fondo.png',
-			website: 'https://www.instagram.com/amiguesporlascalles/',
-			donationUrl: 'https://www.instagram.com/amiguesporlascalles/',
-		},
-		{
-			id: 6,
-			name: 'Difusión V',
-			description: 'Difunde Veganismo.',
-			logo: './difu-v.png',
-			website: 'https://www.difusionv.com',
-			donationUrl: 'https://www.unicef.org/argentina/donar',
-		},
-		{
-			id: 7,
-			name: 'Amigues por las calles',
-			description:
-				'Organización de voluntaries que proveen comida vegana a la gente en situación de calle.',
-			logo: './amigues-sin-fondo.png',
-			website: 'https://www.instagram.com/amiguesporlascalles/',
-			donationUrl: 'https://www.instagram.com/amiguesporlascalles/',
-		},
-	];
+	organizations;
 
 	return (
 		<>
