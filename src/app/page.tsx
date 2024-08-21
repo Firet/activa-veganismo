553074d05f11d61adb3c0cadf743f2f025a1cae2
Firet <damianfederico@protonmail.com>
2024-08-21 00:38:23 -0300

    Create organization-section component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@
 
 import React from 'react';
 import HeroSection from '@/components/templates/hero-section/hero-section';
-import OrganizationCard from '@/components/organisms/organization-card/organization-card';
+import OrganizationSection from '@/components/templates/organization-section/organization-section';
 import Map from '@/components/organisms/map/map';
 
 export default function Home() {
 	return (
 		<>
 			<HeroSection />
-			<OrganizationCard />
+			<OrganizationSection />
 			<Map />
 		</>
 	);
---
+++ src/components/templates/organization-section/organization-section.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './organization-section.css';
+
+export default function OrganizationSection() {
+
+    return (
+        <section className="organization-section">
+
+            <h1 className='organization-section-title'>Buscá la organización a tu medida</h1>
+        </section>
+    );
+}
