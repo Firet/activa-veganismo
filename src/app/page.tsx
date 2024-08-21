b1cf05320d0f68a9646795b2a5ce7e99282e2d40
Firet <damianfederico@protonmail.com>
2024-08-21 00:22:53 -0300

    Move map component to organisms folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import HeroSection from '@/components/templates/hero-section/hero-section';
 import OrganizationCard from '@/components/organisms/organization-card/organization-card';
-import Map from '@/components/map/map';
+import Map from '@/components/organisms/map/map';
 
 export default function Home() {
 	return (
