5f520d9215984cc36c9e1dbf721666149c1b5d2e
Firet <damianfederico@protonmail.com>
2024-08-21 00:14:35 -0300

    Move footer component to templates folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Quicksand } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/header/header';
-import Footer from '@/components/footer/footer';
+import Footer from '@/components/templates/footer/footer';
 
 const quicksand = Quicksand({ subsets: ['latin'] });
 
