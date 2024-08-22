05675d2eb42550dc1621d73bba6aba13c580d26c
Firet <damianfederico@protonmail.com>
2024-08-22 00:13:51 -0300

    Set responsive columns and font-sizes in about us

--- src/components/templates/about-us/about-us.tsx
+++ src/components/templates/about-us/about-us.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './about-us.css';
 import Image from 'next/image';
-import Link from 'next/link';
 
 export default function AboutUs() {
 
     return (
-        <section className='flex flex-row space-evenly items-center about-us-section'>
-            <div className='flex justify-end p-10 w-1/2'>
+        <section className='grid md:grid-cols-1 lg:grid-cols-2 about-us-section'>
+            <div className='flex justify-end p-10'>
                 <Image
                     src="./about-us.jpg"
                     alt="Voluntarios en contraste con el sol"
@@ -17,21 +16,20 @@ export default function AboutUs() {
                     height={500}
                 />
             </div>
-            <div className="w-1/2">
-                <h1 className='about-us-title'>
+            <div className="">
+                <h1 className='about-us-title font-bold sm:text-xl md:text-xl lg:text-5xl xg:text-5xl'>
                     ¿Quiénes Somos?
                 </h1>
-                <h2 className='about-us-subtitle'>
+                <h2 className='about-us-subtitle sm:text-xl md:text-xl lg:text-3xl xg:text-3xl'>
                     Sobre nosotres
                 </h2>
-                <p className='about-us-text w-1/2'>
+                <p className='about-us-text sm:text-sm lg:text-base xg:text-base'>
                     Un grupo de gente apasionada con muchas ganas de ayudar. Creemos en la liberacion animal
                     y en la justicia social.
                     Nuestro objetivo es crear puentes entre personas con recursos
                     y ganas de ayudar, con espacios que
                     lo necesitan.
                 </p>
-                {/* <Link href='/about-us' className='about-us-call-to-action'>Conocé Más</Link> */}
             </div>
         </section>
     );
