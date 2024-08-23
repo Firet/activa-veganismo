020a0cf0eabeb35591fff48ca1311eea4a1c0e63
Firet <damianfederico@protonmail.com>
2024-08-23 18:20:22 -0300

    Fix typo in organization-section

--- src/components/templates/organization-section/organization-section.tsx
+++ src/components/templates/organization-section/organization-section.tsx
@@ -5,7 +5,7 @@ export default function OrganizationSection() {
 	return (
 		<section id="organizations" className="flex flex-col items-center">
 			<h1 className="text-4xl font-bold text-[color:var(--green-2)]">
-				¿Con cual orga te identificás más?
+				¿Con cuál orga te identificás más?
 			</h1>
 			<OrganizationCard />
 		</section>
