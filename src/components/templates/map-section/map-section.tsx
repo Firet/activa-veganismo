86b1e24f0f2d66a9bbf8edbbbfaa4574406d094f
Firet <damianfederico@protonmail.com>
2024-08-23 11:33:56 -0300

    Increase font titles to 4xl in map and organization sections

--- src/components/templates/map-section/map-section.tsx
+++ src/components/templates/map-section/map-section.tsx
@@ -4,7 +4,7 @@ import Map from '@/components/organisms/map/map';
 export default function MapSection() {
 	return (
 		<section id="map" className="flex flex-col items-center w-full mt-10">
-			<h1 className="text-3xl font-bold text-[color:var(--green-2)] mb-7">
+			<h1 className="text-4xl font-bold text-[color:var(--green-2)] mb-7">
 				Buscá tu organización en el mapa
 			</h1>
 			<Map />
--- src/components/templates/organization-section/organization-section.tsx
+++ src/components/templates/organization-section/organization-section.tsx
@@ -4,7 +4,7 @@ import OrganizationCard from '@/components/organisms/organization-card/organizat
 export default function OrganizationSection() {
 	return (
 		<section id="organizations" className="flex flex-col items-center">
-			<h1 className="text-3xl font-bold text-[color:var(--green-2)]">
+			<h1 className="text-4xl font-bold text-[color:var(--green-2)]">
 				Buscá la organización a tu medida
 			</h1>
 			<OrganizationCard />
