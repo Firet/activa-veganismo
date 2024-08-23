da079f34bbce53047e26383c53fa4e74065d9c60
Firet <damianfederico@protonmail.com>
2024-08-23 18:16:49 -0300

    Apply Malu's feedback about voice and wording

--- src/components/templates/map-section/map-section.tsx
+++ src/components/templates/map-section/map-section.tsx
@@ -5,7 +5,7 @@ export default function MapSection() {
 	return (
 		<section id="map" className="flex flex-col items-center w-full mt-10">
 			<h1 className="text-4xl font-bold text-[color:var(--green-2)] mb-7">
-				Buscá tu organización en el mapa
+				Encontrá una orga que te quede cerca
 			</h1>
 			<Map />
 		</section>
