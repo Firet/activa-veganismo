8c20b3853e6e87f8ca31fde9e3ac390e2479f486
evelyndisa <evelyndisa@icloud.com>
2024-09-01 19:30:44 -0300

    Implemented smooth scrolling for the navigation links

--- src/components/templates/header/header.tsx
+++ src/components/templates/header/header.tsx
@@ -1,11 +1,35 @@
 'use client';
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
-import Link from 'next/link';
 import './header.css';
 
 export default function Header() {
+	const [isScrolled, setIsScrolled] = useState(false);
+
+	const scrollToSection = (sectionId: string, offset: number = 0) => {
+		const section = document.getElementById(sectionId);
+	
+		if (section) {
+			const position = section.offsetTop + offset;
+			window.scrollTo({
+				top: position,
+				behavior: 'smooth'
+			});
+		}
+	};
+	
+	useEffect(() => {
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY;
+            setIsScrolled(scrollPosition > 0);
+        };
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
 	return (
 		<header>
 			<div className="image-container">
@@ -18,10 +42,11 @@ export default function Header() {
 				/>
 			</div>
 			<nav className="navbar flex flex-row justify-around items-center">
-				<Link href="#about-us">Quiénes somos</Link>
-				<Link href="#organizations">Organizaciones</Link>
-				<Link href="#map">Mapa</Link>
+				<a onClick={() => scrollToSection('about-us', -30)}>Quiénes somos</a>
+				<a onClick={() => scrollToSection('organizations', -30)}>Organizaciones</a>
+				<a onClick={() => scrollToSection('map')}>Mapa</a>
 			</nav>
 		</header>
 	);
 }
+
