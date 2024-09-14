9b77889e04bfab7401643ebe3215f45af96075ba
evelyndisa <evelyndisa@icloud.com>
2024-09-14 14:47:15 -0300

    Section about us responsive

--- src/components/templates/about-us/about-us.tsx
+++ src/components/templates/about-us/about-us.tsx
@@ -15,22 +15,22 @@ export default function AboutUs() {
 					alt="Voluntarios en contraste con el sol"
 					className=""
 					unoptimized
-					width={500}
+					width={600}
 					height={500}
 				/>
 			</div>
 			<div className="about-us-container">
-				<h1 className="about-us-title  sm:text-xl md:text-xl lg:text-5xl xg:text-5xl mb-10">
+				<h1 className="about-us-title">
 					¿Q
 					<span className="border-b-2 border-red-500 border-solid">
 						uiénes Somos?
 					</span>
 				</h1>
 				<div className='about-us-info'>
-					<h2 className="about-us-subtitle sm:text-xl md:text-xl lg:text-3xl xg:text-3xl">
+					<h2 className="about-us-subtitle">
 						Sobre nosotres
 					</h2>
-					<p className="about-us-text sm:text-sm lg:text-base xg:text-base">
+					<p className="about-us-text">
 					Un grupo de gente apasionada con muchas ganas de ayudar. Creemos en la liberacion animal y en la justicia social. Nuestro objetivo es crear puentes entre personas con recursos y ganas de ayudar, con espacios que lo necesitan. Queremos despertar conciencia social y compromiso en la comunidad, buscando siempre el bienestar de quienes más lo necesitan. Además, queremos ser el punto de referencia para aquellos que quieren involucrarse y para las organizaciones que necesitan apoyo, ofreciendo un servicio fácil y práctico.
 					</p>
 				</div>
