f4cbafaef826bef09961e1ec5d3bf9fe5d41dbe8
Firet <damianfederico@protonmail.com>
2024-09-17 14:29:08 -0300

    Add async to leaflet script in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script'
 import { Quicksand } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/templates/header/header';
@@ -26,6 +27,7 @@ export default function RootLayout({
 					crossOrigin=""
 				/>
 				<script
+					async
 					src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
 					integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
 					crossOrigin=""
