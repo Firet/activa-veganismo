62cbe08b217fd08b13959606a6b2cacc4dd48635
evelyndisa <evelyndisa@icloud.com>
2024-09-22 20:42:36 -0300

    change styles from the Popup

--- src/components/organisms/openstreetmap/openstreetmap.tsx
+++ src/components/organisms/openstreetmap/openstreetmap.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import organizations from '@/constants/organization-map';
+import './openstreetmap.css'
+import styled from 'styled-components';
 
+const StyledPopup = styled(Popup)`
+  padding: 10px;
+  border-radius: 5px;
+  font-size: 14px;
+
+  .leaflet-popup-content-wrapper {
+    background: #f0f0f0;
+    color: #333;
+    border-radius: 5px;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
+  }
+  
+  /*css para el triangulo: */
+  .leaflet-popup-tip {
+    background: #f0f0f0;
+  }
+
+  /*css para la X: */
+  .leaflet-popup-close-button {
+    margin-top: 15px;
+    margin-right: 15px
+  }
+
+  /*css para etiqueta a: */
+  .leaflet-container a{
+    color: #216a89;
+  }
+`;
 export default function OpenStreetMap() {
 
     const center = {
@@ -28,11 +58,11 @@ export default function OpenStreetMap() {
                     key={index}
                     position={{ lat: location.lat, lng: location.lng }}
                 >
-                    <Popup>
-                        <p>{location.title}</p>
-                        <p>{location.location}</p>
-                        <a href={location.contact} target='_blank'>Contacto</a>
-                    </Popup>
+                    <StyledPopup>
+                            <p className='openstreetmap-title'>{location.title}</p>
+                            <p>{location.location}</p>
+                            <a href={location.contact} target='_blank' className='openstreetmap-contact'>Contacto</a>
+                    </StyledPopup>
                 </Marker>
 
             ))}
