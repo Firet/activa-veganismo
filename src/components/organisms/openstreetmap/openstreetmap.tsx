09ad7a2a668a248bcc332119f7adf198026d44b0
evelyndisa <evelyndisa@icloud.com>
2024-09-22 17:12:26 -0300

    Simplified OpenStreetMap component, mapping data, and created two new files

--- src/components/organisms/openstreetmap/openstreetmap.tsx
+++ src/components/organisms/openstreetmap/openstreetmap.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import organizations from '@/constants/organization-map';
 
 export default function OpenStreetMap() {
 
     const center = {
-        lat: -34.603722, // Latitud de Buenos Aires
-        lng: -58.381592, // Longitud de Buenos Aires
+        lat: -34.603722,
+        lng: -58.381592,
     };
 
-    const locations = [
-        { lat: -34.581063540279516, lng: -58.53425706141999, title: 'Viandas en la Kennedy' },
-        { lat: -34.915131589117976, lng: -57.95489072886467, title: 'La verdurolla' },    // se necesita ubi exacta
-        { lat: -34.60102216113937, lng: -58.384254735760926, title: 'Healthsave Microcentro' },
-        { lat: -34.598455385569615, lng: -58.398248442642064, title: 'Healthsave Palermo' },
-        { lat: -34.61421472524178, lng: -58.428028028972584, title: 'Healthsave Caballito' },
-        { lat: -34.56091580070805, lng: -58.45572727503701, title: 'Healthsave Belgrano' },
-        { lat: -34.62833285318419, lng: -58.46353150374588, title: 'Healthsave Flores' },
-        { lat: -34.44417, lng: -58.5775, title: 'Healthsave San Fernando' },
-        { lat: -34.60367706241109, lng: -58.41101116141914, title: 'Amigues Por Las Calles' },
-    ];
+    const locations = organizations.map(org => ({
+        lat: org.lat, 
+        lng: org.lng,
+        title: org.title,
+        location: org.location,
+        contact: org.contact
+    }));
 
     return (
         <MapContainer center={center} zoom={12} scrollWheelZoom={false}>
@@ -32,7 +29,9 @@ export default function OpenStreetMap() {
                     position={{ lat: location.lat, lng: location.lng }}
                 >
                     <Popup>
-                        {location.title}
+                        <p>{location.title}</p>
+                        <p>{location.location}</p>
+                        <a href={location.contact} target='_blank'>Contacto</a>
                     </Popup>
                 </Marker>
 
