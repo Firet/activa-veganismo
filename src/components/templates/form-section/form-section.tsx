6bcdd32911fc5e6ac6929df1d831eb62c858b30c
evelyndisa <evelyndisa@icloud.com>
2024-09-22 21:57:01 -0300

    Standardize text sizes for constistency and add a span to separate the map section

--- src/components/templates/form-section/form-section.tsx
+++ src/components/templates/form-section/form-section.tsx
@@ -5,6 +5,7 @@ import CustomForm from '@/components/organisms/custom-form/custom-form';
 export default function FormSection() {
     return (
         <section className="form-section">
+            <span className='form-span'></span>
             <h1 className="form-title">
                 ¿Querés sumar tu organización? Dejanos tus datos para recibir más info
             </h1>
