ef53deff1fc45ed455b8c9072995a46f08c9e956
Firet <damianfederico@protonmail.com>
2024-12-05 16:16:21 -0300

    Loop through organization cards

--- src/components/organisms/organization-card/organization-card.tsx
+++ src/components/organisms/organization-card/organization-card.tsx
@@ -3,62 +3,58 @@ import organizations from '../../../constants/organization';
 import Image from 'next/image';
 import './organization-card.css';
 
-export default function OrganizationCard() {
+export default function OrganizationCard({ logo = '', name = '', description = '', website = '', instagram = '' }) {
 	organizations;
 
 	return (
-		<>
-			<div className="organizations-container">
-				<ul className="organizations-list">
-					{organizations.map((organization) => (
-						<li key={organization.id} className="organization-item">
-							<div className="image-container">
-								<Image
-									src={organization.logo}
-									alt="Logo de organización"
-									className="organization-logo"
-									unoptimized
-									width={100}
-									height={24}
-								/>
-							</div>
-							<h3 className="organization-title">
-								{organization.name}
-							</h3>
-							<p className="organization-description">
-								{organization.description}
-							</p>
-							{organization.website !== '' ? (
-								<a
-									href={organization.website}
-									target="_blank"
-									className="organization-link"
-								>
-									Sitio web
-								</a>
-							) : (
-								''
-							)}
-							{organization.instagram !== '' ? (
-								<a
-									href={organization.instagram}
-									target="_blank"
-								>
-									<Image
-										src="./instagram.png"
-										alt="Dirigite al instagram de {organization.name}"
-										unoptimized
-										width={24}
-										height={24}
-									/>
-								</a>
-							) : (
-								''
-							)}
-						</li>
-					))}
-				</ul>
-			</div>
-		</>
+		<div className='organizations-container'>
+			<ul className='organizations-list'>
+				<li className="organization-item">
+					<div className="image-container">
+						<Image
+							src={logo}
+							alt="Logo de organización"
+							className="organization-logo"
+							unoptimized
+							width={100}
+							height={24}
+						/>
+					</div>
+					<h3 className="organization-title">
+						{name}
+					</h3>
+					<p className="organization-description">
+						{description}
+					</p>
+					{website !== '' ? (
+						<a
+							href={website}
+							target="_blank"
+							className="organization-link"
+						>
+							Sitio web
+						</a>
+					) : (
+						''
+					)}
+					{instagram !== '' ? (
+						<a
+							href={instagram}
+							target="_blank"
+						>
+							<Image
+								src="./instagram.png"
+								alt="Dirigite al instagram de {name}"
+								unoptimized
+								width={24}
+								height={24}
+							/>
+						</a>
+					) : (
+						''
+					)}
+				</li>
+			</ul>
+		</div>
 	);
 }
--- src/components/templates/organization-section/organization-section.tsx
+++ src/components/templates/organization-section/organization-section.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import OrganizationCard from '@/components/organisms/organization-card/organization-card';
+import organizations from '../../../constants/organization';
+
 
 export default function OrganizationSection() {
+	organizations;
+
 	return (
 		<section id="organizations" className="flex flex-col items-center">
 			<h1 className="organizations-main-title">
 				¿Con cuál organización te identificás más?
 			</h1>
-			<OrganizationCard />
+			{organizations.map((organization) =>
+				<OrganizationCard
+					key={organization.id}
+					logo={organization.logo}
+					name={organization.name}
+					description={organization.description}
+					website={organization.website}
+					instagram={organization.website}
+				/>)}
 		</section>
 	);
 }
