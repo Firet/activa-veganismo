cd725fa9d1c198b9da87f3406a60ed92305009ef
Firet <damianfederico@protonmail.com>
2024-12-06 13:17:55 -0300

    Fix organizationCards styles

--- src/components/organisms/organization-card/organization-card.tsx
+++ src/components/organisms/organization-card/organization-card.tsx
@@ -7,54 +7,50 @@ export default function OrganizationCard({ logo = '', name = '', description = '
 	organizations;
 
 	return (
-		<div className='organizations-container'>
-			<ul className='organizations-list'>
-				<li className="organization-item">
-					<div className="image-container">
-						<Image
-							src={logo}
-							alt="Logo de organización"
-							className="organization-logo"
-							unoptimized
-							width={100}
-							height={24}
-						/>
-					</div>
-					<h3 className="organization-title">
-						{name}
-					</h3>
-					<p className="organization-description">
-						{description}
-					</p>
-					{website !== '' ? (
-						<a
-							href={website}
-							target="_blank"
-							className="organization-link"
-						>
-							Sitio web
-						</a>
-					) : (
-						''
-					)}
-					{instagram !== '' ? (
-						<a
-							href={instagram}
-							target="_blank"
-						>
-							<Image
-								src="./instagram.png"
-								alt="Dirigite al instagram de {name}"
-								unoptimized
-								width={24}
-								height={24}
-							/>
-						</a>
-					) : (
-						''
-					)}
-				</li>
-			</ul>
-		</div>
+		<section className='organization-item'>
+			<div className="image-container">
+				<Image
+					src={logo}
+					alt="Logo de organización"
+					className="organization-logo"
+					unoptimized
+					width={100}
+					height={24}
+				/>
+			</div>
+			<h3 className="organization-title">
+				{name}
+			</h3>
+			<p className="organization-description">
+				{description}
+			</p>
+			{website !== '' ? (
+				<a
+					href={website}
+					target="_blank"
+					className="organization-link"
+				>
+					Sitio web
+				</a>
+			) : (
+				''
+			)}
+			{instagram !== '' ? (
+				<a
+					href={instagram}
+					target="_blank"
+				>
+					<Image
+						src="./instagram.png"
+						alt="Dirigite al instagram de {name}"
+						unoptimized
+						width={24}
+						height={24}
+					/>
+				</a>
+			) : (
+				''
+			)}
+		</section>
 	);
 }
--- src/components/templates/organization-section/organization-section.tsx
+++ src/components/templates/organization-section/organization-section.tsx
@@ -7,19 +7,21 @@ export default function OrganizationSection() {
 	organizations;
 
 	return (
-		<section id="organizations" className="flex flex-col items-center">
+		<>
 			<h1 className="organizations-main-title">
 				¿Con cuál organización te identificás más?
 			</h1>
-			{organizations.map((organization) =>
-				<OrganizationCard
-					key={organization.id}
-					logo={organization.logo}
-					name={organization.name}
-					description={organization.description}
-					website={organization.website}
-					instagram={organization.website}
-				/>)}
-		</section>
+			<section id="organizations" className="flex flex-wrap organizations-container">
+				{organizations.map((organization) =>
+					<OrganizationCard
+						key={organization.id}
+						logo={organization.logo}
+						name={organization.name}
+						description={organization.description}
+						website={organization.website}
+						instagram={organization.website}
+					/>)}
+			</section>
+		</>
 	);
 }
